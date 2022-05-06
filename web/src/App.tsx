3432f6445740c80e654dd15a5f9d45046d04ce79
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-05-06 08:08:24 -0300

    adding popover at widget

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,29 +1,7 @@
-interface ButtonProps {
-  text?: string;
-}
+import { Widget } from './components/Widget'
 
-function Button(props: ButtonProps) {
+export function App() {
   return (
-    <button
-      className='bg-violet-500 
-                px-4 h-10 rounded 
-              first-letter:text-violet-100 
-              hover:bg-violet-700
-                transition-colors'
-    >
-      {props.text ?? 'Default'}
-    </button>
+    <Widget />
   );
 }
-
-function App() {
-  return (
-    <div className='flex gap-2'>
-      <Button text='Ok' />
-      <Button text='Send' />
-      <Button />
-    </div>
-  );
-}
-
-export default App;
